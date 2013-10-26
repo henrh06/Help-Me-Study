ed6eee9299964c863607fde3afde88461cfed836
Henrik Heggland <Henrik@Henrik-Hegglands-iMac.local>
2013-10-26 14:37:05 +0200

    Initial project files

---
+++ HMS/HMS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HMS
+//
+//  Created by Henrik Heggland on 26.10.13.
+//  Copyright (c) 2013 Henrik Heggland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
